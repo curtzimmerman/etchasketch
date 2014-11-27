c3c30b10f915248b486656df8849b34b4f1d293d
czimmerman9 <czimmerman9@gmail.com>
2014-11-27 05:06:02 -0500

    updated functionality

--- js/etchasketch.js
+++ js/etchasketch.js
@@ -1,21 +1,44 @@
+
+// Returns the length of a div's side to fit inside the wrapper
 function squareSize(numPerRow) {
 	var height = $("#wrapper").height();
-	return +height / +numPerRow; 
+	return height / numPerRow; 
 }
 
-function createDivs() {
-	//ask user for number of squares, make width of divs according to what will fit in the wrapper
-	var numDivs = 64;
-	var numPerRow = parseInt(Math.sqrt(+numDivs));
-	numDivs = Math.pow(numPerRow, 2); //gets largest perfect square less than numDivs	
-	
+//creates all etchasketch squares that fit inside the wrapper
+function initializeDivs(numPerSide) {	
 	var wrapper = $("#wrapper");
-	var size = squareSize(numPerRow);
-	for (var i=0; i < numDivs; i++) {
-		wrapper.append("<div id=\""+i+"\"></div>");
-		$("#"+i).css({"height":size+"px","width":size+"px", "float":"left", "vertical-align": "middle"});
+	wrapper.empty();
+	var size = squareSize(numPerSide);
+	var count = 0;
+	for (var i=0; i < numPerSide; i++) {
+		for (var j=0; j < numPerSide; j++) {
+		wrapper.append("<div id=\""+i+"\" class=\"canvasSquare\"></div>");
+		count++;
+		}
 	}
-	wrapper.children().on("mouseover", function() {
+	$(".canvasSquare").width(size);
+	$(".canvasSquare").height(size);
+	$(".canvasSquare").css({"float": "left", "border":"none", "margin": "0px auto"})
+	selectMode();
+}
+
+//changes background color of divs to white
+function defaultEtch() {
+	$("#wrapper").children().off("mouseover");
+	$("#wrapper").children().on("mouseover", function() {
+		var color = "#FFFFFF";
+		if (!$(this).hasClass("highlighted")){
+			$(this).css({"background-color": color});
+			$(this).addClass("highlighted");
+		}
+	});
+}
+
+//changes background color of divs to random colors
+function randomColors() {
+	$("#wrapper").children().off("mouseover");
+	$("#wrapper").children().on("mouseover", function() {
 		var color = 0;
 		color = '#'+Math.random().toString(16).substr(-6);
 		if (!$(this).hasClass("highlighted")){
@@ -25,15 +48,37 @@ function createDivs() {
 	});
 }
 
+//user entered number of divs per side of wrapper
+function getSides() {
+	do {
+		var num = prompt("How many squares per side? (Between 8 and 100", 8);
+		return num;
+	}
+	while (num <= 8 && num >= 100)
+}
 
-
+function selectMode() {
+	if($("input[name=color]:checked").val() == "normal") {
+		defaultEtch();
+	}
+	else if ($("input[name=color]:checked").val() == "random") {
+		randomColors();
+	}
+}
 
 $(document).ready(function() {
-	$("#header").after("<div id=\"wrapper\"></div>");
-	$("#wrapper").css({"height":"960px","width":"960px","background-color":"#000000"})
-	createDivs();	
+	$("#options").after("<div id=\"wrapper\"></div>");
+	$("#wrapper").css({"height":"750px","width":"750px","background-color":"#000000", "display": "inline-block"})
+	initializeDivs(8);
 	$("#clearButton").on("click", function (){
-		$("#wrapper").empty();
-		createDivs();
+		$(".canvasSquare").css({"background-color":"#000000"});
+		$(".canvasSquare").removeClass("highlighted");
+		selectMode();
 	});
+	$(".mode").change(function() {
+		selectMode();
+	});
+	$("#resize").on("click", function() {
+		initializeDivs(+getSides());
+	})
 });
